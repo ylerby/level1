2941dedd838e9317c0ced6288f5f16c75da5d0ae
Ylerby <WeaklyFob@yandex.ru>
2023-12-14 01:47:16 +0300

    выполнено задание 21

---
+++ tasks/task21/task21.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+func Add(a *big.Int, b *big.Int) *big.Int {
+	result := new(big.Int)
+	return result.Add(a, b)
+}
+
+func Sub(a *big.Int, b *big.Int) *big.Int {
+	result := new(big.Int)
+	return result.Sub(a, b)
+}
+
+func Mul(a *big.Int, b *big.Int) *big.Int {
+	result := new(big.Int)
+	return result.Mul(a, b)
+}
+
+func Div(a *big.Int, b *big.Int) *big.Int {
+	result := new(big.Int)
+	return result.Div(a, b)
+}
+
+func main() {
+	a := big.NewInt(0)
+	b := big.NewInt(0)
+
+	// указываем значение из строки, так как задать значение напрямую не получится, возникнет ошибка
+	// переполнения
+	a.SetString("600000000000000000000", 10)
+	b.SetString("300000000000000000000", 10)
+
+	fmt.Printf("Результат сложения: %v\n", Add(a, b))
+	fmt.Printf("Результат вычитания: %v\n", Sub(a, b))
+	fmt.Printf("Результат умножения: %v\n", Mul(a, b))
+	fmt.Printf("Результат деления: %v\n", Div(a, b))
+}
