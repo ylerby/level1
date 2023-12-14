6df993ff75f9263875ee12b4ecbe439fd369e8a2
Ylerby <WeaklyFob@yandex.ru>
2023-12-14 16:52:14 +0300

    выполнено задание 24

---
+++ tasks/task24/task24.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Point - структура содержащая координаты точки типа float64
+type Point struct {
+	x,
+	y float64
+}
+
+// New - "конструктор" для типа Point. Создает новый объект типа Point
+func New(newX, newY float64) Point {
+	return Point{
+		x: newX,
+		y: newY,
+	}
+}
+
+// distance - метод структуры Point, принимает объект типа Point (вторая точка),
+// возвращает расстояние между точками
+func (p Point) distance(secondPoint Point) float64 {
+	xDifference := p.x - secondPoint.x
+	yDifference := p.y - secondPoint.y
+
+	// расстояние между точками вычисляется как √((x1 - x2)ˆ2 + (y1 - y2)ˆ2)
+	distance := math.Sqrt(math.Pow(xDifference, 2) + math.Pow(yDifference, 2))
+	return distance
+}
+
+func main() {
+	firstPoint := New(14.4, 64.2)
+	secondPoint := New(-54.4, 194.2)
+
+	result := firstPoint.distance(secondPoint)
+	fmt.Printf("Расстояние между точками: %f\n", result)
+}
